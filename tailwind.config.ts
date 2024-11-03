f5fd23ea4dca032c1897db415ec3a3fbd5940660
Terry Sutton <saltcod@gmail.com>
2024-11-03 14:03:53 -0330

    Run knip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
-        },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
